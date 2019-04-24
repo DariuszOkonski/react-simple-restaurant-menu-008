143f7de4363f48454883c3c0fe124cb421d802a7
Dariusz Okonski <darek200180@gmail.com>
2019-04-24 21:37:35 +0100

    Updates

---
+++ precache-manifest.5da8867586dcee1b79f67ebbcaacd09f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deca59fd5d94750eace1001409698ffc",
+    "url": "/react-simple-restaurant-menu-008/index.html"
+  },
+  {
+    "revision": "1d1c71e8bc52d172ecfd",
+    "url": "/react-simple-restaurant-menu-008/static/css/main.59d3fc70.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-simple-restaurant-menu-008/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1d1c71e8bc52d172ecfd",
+    "url": "/react-simple-restaurant-menu-008/static/js/main.dcc1e81a.chunk.js"
+  },
+  {
+    "revision": "4667d0830d50fe8ba54f",
+    "url": "/react-simple-restaurant-menu-008/static/js/runtime~main.581f3c9f.js"
+  }
+]);
